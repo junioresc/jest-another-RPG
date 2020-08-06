71ca7e452953662eebd694d5857496be9a45459e
Junioresc <junioresc1092@gmail.com>
2020-08-05 20:14:46 -0400

    added reduceHealth, isAlive, and getHealth functions to the Player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -32,3 +32,32 @@ test(`gets inventory from player or returns false`, () => {
 
     expect(player.getInventory()).toEqual(false);
 });
+
+test(`gets player's health value`, () => {
+    const player = new Player(`Dave`);
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test(`checks if player is alive or not`, () => {
+    const player = new Player(`Dave`);
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test(`subtracts from player's health`, () => {
+    const player = new Player(`Dave`);
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
